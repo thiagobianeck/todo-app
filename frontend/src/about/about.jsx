87a30b11a5b3c5dea9cbf788dc7be4bb5532ae09
Thiago Bianeck <thiagobianeck@gmail.com>
2019-09-18 22:16:07 -0300

    Componentes TodForm e TodoList

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'/>
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, dolorem eaque expedita facilis fugiat id illum ipsa labore laborum optio sed sint sunt veritatis voluptatum!</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, dolorem eaque expedita facilis fugiat id illum ipsa labore laborum optio sed sint sunt veritatis voluptatum!</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, dolorem eaque expedita facilis fugiat id illum ipsa labore laborum optio sed sint sunt veritatis voluptatum!</p>
+  </div>
+);
